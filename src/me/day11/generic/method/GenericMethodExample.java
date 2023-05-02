2069d8c67190035fe2b0f106e5bb2945b81ad2d4
echoiing <qwertyuiop7823@gmail.com>
2023-05-02 18:01:44 +0900

    Feat: Add code

--- src/me/day11/generic/method/GenericMethodExample.java
+++ src/me/day11/generic/method/GenericMethodExample.java
@@ -1,30 +1,34 @@
 package me.day11.generic.method;//package me.day11.generic.method;
-//
-//import me.day11.collection.map.treemap.value.Pen;
-//import me.day11.generic.method.decoration.Decorations;
-//
-//public class GenericMethodExample {
-//    public static void main(String[] args) {
-//        Pen pen = new Pen("1001", "모닝글로리", "빨강");
-//
-//        Box<Pen> boxingPen = Utils.boxing(pen);
-//        System.out.println("boxingPen = " + boxingPen);
-//
-//        Utils.decoration(boxingPen, new Decorations(new Decoration[]{new Ribbon()}));
-//
-//        Pen unboxingPen = Utils.unboxing(boxingPen);
-//        System.out.println("unboxingPen = " + unboxingPen);
-//        System.out.println();
-//
-//        //////////////////////////////////////////////////////////////////
-//        Note note = new Note("1001", "모닝글로리", true);
-//
-//        Box<Note> boxingNote = Utils.boxing(note);
-//        System.out.println("boxingNote = " + boxingNote);
-//
-//        Utils.decoration(boxingPen, new Decorations(new Decoration[]{new DryFlower()}));
-//
-//        Note unboxingNote = Utils.unboxing(boxingNote);
-//        System.out.println("unboxingNote = " + unboxingNote);
-//    }
-//}
+
+import me.day11.collection.map.treemap.value.Pen;
+import me.day11.collection.use.gift.Note;
+import me.day11.generic.method.decoration.Decoration;
+import me.day11.generic.method.decoration.Decorations;
+import me.day11.generic.method.decoration.DryFlower;
+import me.day11.generic.method.decoration.Ribbon;
+
+public class GenericMethodExample {
+    public static void main(String[] args) {
+        Pen pen = new Pen("1001", "모닝글로리", "빨강");
+
+        Box<Pen> boxingPen = Utils.boxing(pen);
+        System.out.println("boxingPen = " + boxingPen);
+
+        Utils.decoration(boxingPen, new Decorations(new Decoration[]{new Ribbon()}));
+
+        Pen unboxingPen = Utils.unboxing(boxingPen);
+        System.out.println("unboxingPen = " + unboxingPen);
+        System.out.println();
+
+        //////////////////////////////////////////////////////////////////
+        Note note = new Note("1001", "모닝글로리", true);
+
+        Box<Note> boxingNote = Utils.boxing(note);
+        System.out.println("boxingNote = " + boxingNote);
+
+        Utils.decoration(boxingPen, new Decorations(new Decoration[]{new DryFlower()}));
+
+        Note unboxingNote = Utils.unboxing(boxingNote);
+        System.out.println("unboxingNote = " + unboxingNote);
+    }
+}
--- src/me/day11/generic/wildcard/course/CourseUtils.java
+++ src/me/day11/generic/wildcard/course/CourseUtils.java
@@ -5,12 +5,8 @@ import me.day11.generic.wildcard.course.register.Animal;
 import me.day11.generic.wildcard.course.register.Person;
 
 public interface CourseUtils {
-//    static <T extends Person> void registerPersonCourse(Course<T> course) { // Course<Person> .....
-//        System.out.println(course + " is registered successfully.\n\n");
-//    }
-//    static <T extends Animal> void registerAnimalCourse(Course<T> course) { // Course<Animal>, Course<Cat>, Course<Dog>
-//        System.out.println(course + " is registered successfully.\n\n");
-//    }
+    // @TODO: wildcard 안 쓰는 함수로 바꿔보기
+
 
     static void registerPersonCourse(Course<? extends Person> course) {
         System.out.println(course + " is registered successfully.\n\n");
