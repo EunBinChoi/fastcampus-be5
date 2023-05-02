88430a4a889773f55b349f803391d1c5dcbba7e0
echoiing <qwertyuiop7823@gmail.com>
2023-05-02 21:53:15 +0900

    Feat: Add code

--- src/me/day11/generic/wildcard/course/CourseUtils.java
+++ src/me/day11/generic/wildcard/course/CourseUtils.java
@@ -5,7 +5,6 @@ import me.day11.generic.wildcard.course.register.Animal;
 import me.day11.generic.wildcard.course.register.Person;
 
 public interface CourseUtils {
-    // @TODO: wildcard 안 쓰는 함수로 바꿔보기
 
     static <T extends Person> void registerPersonCourse(Course<T> course) {
         System.out.println(course + " is registered successfully.\n\n");
